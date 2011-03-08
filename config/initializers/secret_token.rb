cd8348e3af38a527a73bf6ef29d1ed8f6a776b62
Vince Vincent <mellterm@gmail.com>
2011-03-08 13:38:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Transentrydemo::Application.config.secret_token = '209c8aeb3a961eecf318bb44bdc12fcb870949b4080f119c22480d634283856346a7771d3d4c6999552dbe5f82bdd97589f906689fc1b7cf3668d608cedbcfcf'
